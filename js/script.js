f24f19656b85d41d0ad2ae94b7ad1592d5cf339b
marthebull <marthebullpettersen@gmail.com>
2022-01-07 13:40:26 +0100

    oppgaver

--- js/script.js
+++ js/script.js
@@ -1,18 +1,64 @@
-console.log("Hello, try the console!");
-
-// 1
-
-var num = 8;
-
-var myBool = true;
-
-var quote = "This is me, this is real";
+console.log("Hello");
 
 
-// 2 
 
-var firstName = "Marthe Bull ";
+// Question 1
+
+var age = 25;
+
+var tryingToLearn = true;
+
+var text = "And my name is ";
+
+
+
+// Question 2 
+
+var firstName = "Marthe Bull";
 
 var lastName = "Pettersen";
 
-console.log (firstName + lastName);
+var fullName = firstName + " " + lastName;
+
+console.log (fullName);
+
+// Console: Marthe Bull Pettersen
+
+
+
+// Question 3 
+
+var animal = "Frog";
+
+console.log (typeof animal);
+
+// Console: string
+
+var frog = "string";
+
+console.log ('The type og "frog" is ' + frog);
+
+// Console: The type of "frog" is string
+
+
+
+// Question 4
+
+var orderHasShipped = false;
+
+if (orderHasShipped === true) {
+    console.log ("The order shipped.")
+}
+else {
+    console.log ("The order did not ship.")
+}
+
+// Console: The order did not ship.
+
+
+
+// Question 5
+
+for (var i = 7; i <= 13; i++) {
+    console.log ("The number is :" + i)
+}
