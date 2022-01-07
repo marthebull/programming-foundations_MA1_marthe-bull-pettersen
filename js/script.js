05be7d3591fec61bfe39b3d93c4d0cbdbc717b7f
marthebull <marthebullpettersen@gmail.com>
2022-01-07 13:01:13 +0100

    fds

---
+++ js/script.js
@@ -0,0 +1,18 @@
+console.log("Hello, try the console!");
+
+// 1
+
+var num = 8;
+
+var myBool = true;
+
+var quote = "This is me, this is real";
+
+
+// 2 
+
+var firstName = "Marthe Bull ";
+
+var lastName = "Pettersen";
+
+console.log (firstName + lastName);
